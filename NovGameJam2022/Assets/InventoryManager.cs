83507263c1b0f80b21be9e06a5310f17440f76ad
XRKun12 <78513984+XRKun12@users.noreply.github.com>
2022-11-20 16:51:12 -0800

    added player controller to unity

---
+++ NovGameJam2022/Assets/InventoryManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public bool hasPlate = false;
+    public Text collectable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
